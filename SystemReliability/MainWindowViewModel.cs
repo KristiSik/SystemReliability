6cd23767d722d7067b163372bd2394ebd6a6e22d
Kristian Sik <ksi@scada-international.com>
2019-12-15 17:54:28 +0200

    Add project files.

---
+++ SystemReliability/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace SystemReliability
+{
+    public class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Time { get; set; }
+
+        public string Lambda0 { get; set; }
+
+        public string Lambda1 { get; set; }
+
+        public string Lambda2 { get; set; }
+
+        public string Lambda3 { get; set; }
+
+        public string Beta { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
